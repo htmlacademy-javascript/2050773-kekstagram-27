178e63e8ae1227eda20a6e85b789c00f0dcfcee7
Anastasiia Chasovskikh <nastya.chasovskikh@gmail.com>
2022-11-17 12:09:57 +0400

    setLikesClick, setRandomClick

--- js/create-previews.js
+++ js/create-previews.js
@@ -1,9 +1,41 @@
 import {showBigPicture} from './create-big-picture.js';
 
+const buttonFilteredByLikes = document.querySelector('#filter-discussed');
+const buttonFilteredRandom = document.querySelector('#filter-random');
+const RANDOM_PICS_AMOUNT = 10;
+
+
+const compareLikes = (picA, picB) => {
+  const likeA = picA.likes;
+  const likeB = picB.likes;
+
+  return likeB - likeA;
+};
+
+const setLikesClick = (data, cb) => {
+  buttonFilteredByLikes.addEventListener('click', () => {
+    const sortedData = data.slice().sort(compareLikes);
+    cb(sortedData);
+  });
+};
+
+const getRandomPhotosArray = (data) => {
+  const newArray = data.slice(0, RANDOM_PICS_AMOUNT);
+  return newArray.sort(() => Math.round(Math.random() * 100) - 50);
+};
+
+const setRandomClick = (data, cb) => {
+  buttonFilteredRandom.addEventListener('click', () => {
+    cb(getRandomPhotosArray(data));
+  });
+};
+
 const createPreviews = (data) => {
   const picturesList = document.querySelector('.pictures');
   const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
   const similarUserFragment = document.createDocumentFragment();
+  const imageFilters = document.querySelector('.img-filters');
+
 
   data.forEach(({url, likes, comments, description}, index) => {
     const userPicture = pictureTemplate.cloneNode(true);
@@ -20,6 +52,7 @@ const createPreviews = (data) => {
     });
   });
   picturesList.appendChild(similarUserFragment);
+  imageFilters.classList.remove('img-filters--inactive');
 };
 
-export {createPreviews};
+export {createPreviews, setLikesClick, setRandomClick};
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {createPreviews} from './create-previews.js';
+import {createPreviews, setLikesClick, setRandomClick} from './create-previews.js';
 import {initForm, closeEditForm, initFormSubmit} from './form.js';
 import {initEffects} from './effects.js';
 import {initScale} from './scale.js';
@@ -9,6 +9,11 @@ initForm();
 initEffects();
 initScale();
 
-getData((photos) => createPreviews(photos));
+getData((photos) => {
+  createPreviews(photos);
+  setLikesClick(photos, (sortedPhotos) => createPreviews(sortedPhotos));
+  setRandomClick(photos, (sortedPhotos) => createPreviews(sortedPhotos));
+});
+// getData((photos) => createPreviews(SortDataByLikes(photos)));
 
 initFormSubmit(closeEditForm);
